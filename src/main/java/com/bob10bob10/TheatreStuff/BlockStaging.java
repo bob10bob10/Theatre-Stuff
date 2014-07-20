8877e037280b80df81d28686d667b5c19d25b30d
bob10bob10 <bob10bob10teclab@gmail.com>
2014-07-20 19:31:15 +0100

    0.1
    First Version!

---
+++ src/main/java/com/bob10bob10/TheatreStuff/BlockStaging.java
@@ -0,0 +1,12 @@
+package com.bob10bob10.TheatreStuff;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockStaging extends Block {
+
+	protected BlockStaging(Material material) {
+		super(material);
+	}
+
+}
---
+++ src/main/java/com/bob10bob10/TheatreStuff/ItemMicrophone.java
@@ -0,0 +1,7 @@
+package com.bob10bob10.TheatreStuff;
+
+import net.minecraft.item.Item;
+
+public class ItemMicrophone extends Item{
+
+}
